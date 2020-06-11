6b9720ccbf2e4879b8a7fd279892c7a9aa22f29c
Fitsum A <fitsum@gmail.com>
2020-06-12 01:17:50 +0300

    Adding the calculation for the three values ..

--- Loan  Calculater/assets/js/app.js
+++ Loan  Calculater/assets/js/app.js
@@ -1,11 +1,12 @@
 //Adding Ev List to the calc button
-document.querySelector('#calc').addEventListener('click', calculateLoan);
+document.querySelector('#form-sub').addEventListener('submit', calculateLoan);
 //Defining the cal method 
 
 function calculateLoan(e)
 {
+    'use strict';
     // Get UI Variables 
-    const amount = document.querySelector('#amount');
+    const amount = document.getElementById('amount');
     const interest = document.querySelector('#interest');
     const year = document.querySelector('#year');
     const monthlyPayment = document.querySelector('#mp');
@@ -13,16 +14,24 @@ function calculateLoan(e)
     const totalInterest = document.querySelector('#ti');
 
     // Calculations 
-    
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value)/100/12;
+    const calculatedPayment = parseFloat(year.value)*12;
 
+    // Monthly payment
+    const cs = Math.pow(1+calculatedInterest, calculatedPayment);
+    const monthly = (principal*cs*calculatedInterest)/(cs-1);
 
-
-
-
-
-
-
-//    console.log('Calculating ...');
+    if(isFinite(monthly))
+    {
+     monthlyPayment.value = monthly.toFixed(2);
+     totalPayment.value = (monthly*calculatedPayment).toFixed(2);
+     totalInterest.value = ((monthly*calculatedPayment) - principal).toFixed(2);
+    }
+    else
+    {
+      console.log('Somthing wrong....');
+    }
  
   e.preventDefault();
 }
