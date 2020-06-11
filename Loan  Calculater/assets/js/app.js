27d611ed91858a6b649dccf5b57a3e407dab9366
Fitsum A <fitsum@gmail.com>
2020-06-12 00:09:41 +0300

    Adding Button even listener and Declaring UI Variables

---
+++ Loan  Calculater/assets/js/app.js
@@ -0,0 +1,28 @@
+//Adding Ev List to the calc button
+document.querySelector('#calc').addEventListener('click', calculateLoan);
+//Defining the cal method 
+
+function calculateLoan(e)
+{
+    // Get UI Variables 
+    const amount = document.querySelector('#amount');
+    const interest = document.querySelector('#interest');
+    const year = document.querySelector('#year');
+    const monthlyPayment = document.querySelector('#mp');
+    const totalPayment = document.querySelector('#tp');
+    const totalInterest = document.querySelector('#ti');
+
+    // Calculations 
+    
+
+
+
+
+
+
+
+
+//    console.log('Calculating ...');
+ 
+  e.preventDefault();
+}
