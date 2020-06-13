17f4bc037fe9058bf3f8459a32c94354b97da42f
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-14 04:35:16 +0530

    Adding the script tag for es6 and 5

--- Library Catalog/assets/js/es5_Script.js
+++ Library Catalog/assets/js/es5_Script.js
@@ -1,112 +1,112 @@
-// Book Constructor 
-function Book(title, author, isbn)
-{
-      this.title = title;
-      this.author = author;
-      this.isbn = isbn;
-
-}
-
-// UI
-function UI() {}
-
-UI.prototype.addBookToList = function(book)
-{
-
-   const bookList = document.querySelector('#bookList');
-//    create elemet on the table 
-const singleRow = document.createElement('tr');
-singleRow.innerHTML = `
-          <td>${book.title}</td>
-          <td>${book.author}</td>
-          <td>${book.isbn}</td>
-          <td><a href="#" class='delete'>x</a></td>
-    `;
-
-bookList.appendChild(singleRow);
-// console.log(singleRow);
-
-}
-UI.prototype.clearAll = function()
-{
-    document.querySelector('#title').value = ''; 
-    document.querySelector('#author').value = ''; 
-    document.querySelector('#isbn').value = ''; 
-}
-// Adding show Allert function to prototype
-UI.prototype.showMessage = function(msg,type)
-{
-    //UI Variables 
-    const container = document.querySelector('.container');
-    const form = document.querySelector('#book');
-//    Create the div 
-    const div = document.createElement('div');
-    div.className = `alert ${type}`;
-    div.appendChild(document.createTextNode(msg));
-
-    container.insertBefore(div,form);
-
-//   set Timer for the alert to disapear 
-    setTimeout(function(){
-        document.querySelector('.alert').remove();
-         },1000);
-
-}
-// delete books 
-UI.prototype.deletSingleBook = function(target)
-{
-  
-    if(target.className === 'delete')
+    // Book Constructor 
+    function Book(title, author, isbn)
     {
-      target.parentElement.parentElement.remove();
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+
     }
 
-}
-// Even Listeners 
-document.querySelector('#book').addEventListener('submit', addBook);
+    // UI
+    function UI() {}
 
-function addBook(e)
-{
-// Getting the UI values 
-const bTitle = document.querySelector('#title').value; 
-const bAuthor = document.querySelector('#author').value; 
-const bIsbn = document.querySelector('#isbn').value; 
+    UI.prototype.addBookToList = function(book)
+    {
 
-// Create a Book 
-const book = new Book(bTitle,bAuthor,bIsbn);
+    const bookList = document.querySelector('#bookList');
+    //    create elemet on the table 
+    const singleRow = document.createElement('tr');
+    singleRow.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class='delete'>x</a></td>
+        `;
 
-// Pass to UI 
-const ui = new UI();
-// if the fields are empty 
-if(bTitle ===  '' || bAuthor === '' || bIsbn === '')
-{
-      ui.showMessage('All Fields are Required !!!','err');
-}
-else
-{
-    ui.addBookToList(book);
-     ui.showMessage('Book Added Successfully !!!','success');
-    ui.clearAll();
-}
+    bookList.appendChild(singleRow);
+    // console.log(singleRow);
+
+    }
+    UI.prototype.clearAll = function()
+    {
+        document.querySelector('#title').value = ''; 
+        document.querySelector('#author').value = ''; 
+        document.querySelector('#isbn').value = ''; 
+    }
+    // Adding show Allert function to prototype
+    UI.prototype.showMessage = function(msg,type)
+    {
+        //UI Variables 
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book');
+    //    Create the div 
+        const div = document.createElement('div');
+        div.className = `alert ${type}`;
+        div.appendChild(document.createTextNode(msg));
+
+        container.insertBefore(div,form);
+
+    //   set Timer for the alert to disapear 
+        setTimeout(function(){
+            document.querySelector('.alert').remove();
+            },1000);
+
+    }
+    // delete books 
+    UI.prototype.deletSingleBook = function(target)
+    {
+    
+        if(target.className === 'delete')
+        {
+        target.parentElement.parentElement.remove();
+        }
+
+    }
+    // Even Listeners 
+    document.querySelector('#book').addEventListener('submit', addBook);
+
+    function addBook(e)
+    {
+    // Getting the UI values 
+    const bTitle = document.querySelector('#title').value; 
+    const bAuthor = document.querySelector('#author').value; 
+    const bIsbn = document.querySelector('#isbn').value; 
+
+    // Create a Book 
+    const book = new Book(bTitle,bAuthor,bIsbn);
+
+    // Pass to UI 
+    const ui = new UI();
+    // if the fields are empty 
+    if(bTitle ===  '' || bAuthor === '' || bIsbn === '')
+    {
+        ui.showMessage('All Fields are Required !!!','err');
+    }
+    else
+    {
+        ui.addBookToList(book);
+        ui.showMessage('Book Added Successfully !!!','success');
+        ui.clearAll();
+    }
 
 
-// console.log(book);
+    // console.log(book);
 
-e.preventDefault();
-}
-
-// Adding delete 
-document.querySelector('#bookList').addEventListener('click', deletBook);
-
-function deletBook(e)
-{
-// Pass to UI 
-const ui = new UI();
-if(e.target.className === 'delete')
-{
-    ui.deletSingleBook(e.target);
-    ui.showMessage('Book Removed!!','success');
     e.preventDefault();
-}
+    }
 
-}
+    // Adding delete 
+    document.querySelector('#bookList').addEventListener('click', deletBook);
+
+    function deletBook(e)
+    {
+    // Pass to UI 
+    const ui = new UI();
+    if(e.target.className === 'delete')
+    {
+        ui.deletSingleBook(e.target);
+        ui.showMessage('Book Removed!!','success');
+        e.preventDefault();
+    }
+
+    }
---
+++ Library Catalog/assets/js/es6_Script.js
@@ -0,0 +1,116 @@
+            class Book
+            {
+            constructor(title, author, isbn)
+            {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+            }
+            }
+
+            class UI
+            {
+                addBookToList(book)
+                {
+                
+                const bookList = document.querySelector('#bookList');
+                //    create elemet on the table 
+                const singleRow = document.createElement('tr');
+                singleRow.innerHTML = `
+                        <td>${book.title}</td>
+                        <td>${book.author}</td>
+                        <td>${book.isbn}</td>
+                        <td><a href="#" class='delete'>x</a></td>
+                    `;
+                
+                bookList.appendChild(singleRow);
+                // console.log(singleRow);
+                
+                }
+                clearAll()
+                {
+                    document.querySelector('#title').value = ''; 
+                    document.querySelector('#author').value = ''; 
+                    document.querySelector('#isbn').value = ''; 
+                }
+                // Adding show Allert function to prototype
+                showMessage(msg,type)
+                {
+                    //UI Variables 
+                    const container = document.querySelector('.container');
+                    const form = document.querySelector('#book');
+                //    Create the div 
+                    const div = document.createElement('div');
+                    div.className = `alert ${type}`;
+                    div.appendChild(document.createTextNode(msg));
+                
+                    container.insertBefore(div,form);
+                
+                //   set Timer for the alert to disapear 
+                    setTimeout(function(){
+                        document.querySelector('.alert').remove();
+                        },1000);
+                
+                }
+                // delete books 
+                deletSingleBook(target)
+                {
+                
+                    if(target.className === 'delete')
+                    {
+                    target.parentElement.parentElement.remove();
+                    }
+                
+                }
+
+            }
+
+
+            // Even Listeners 
+        document.querySelector('#book').addEventListener('submit', addBook);
+
+        function addBook(e)
+        {
+        // Getting the UI values 
+        const bTitle = document.querySelector('#title').value; 
+        const bAuthor = document.querySelector('#author').value; 
+        const bIsbn = document.querySelector('#isbn').value; 
+
+        // Create a Book 
+        const book = new Book(bTitle,bAuthor,bIsbn);
+
+        // Pass to UI 
+        const ui = new UI();
+        // if the fields are empty 
+        if(bTitle ===  '' || bAuthor === '' || bIsbn === '')
+        {
+            ui.showMessage('All Fields are Required !!!','err');
+        }
+        else
+        {
+            ui.addBookToList(book);
+            ui.showMessage('Book Added Successfully !!!','success');
+            ui.clearAll();
+        }
+
+
+        // console.log(book);
+
+        e.preventDefault();
+        }
+
+        // Adding delete 
+        document.querySelector('#bookList').addEventListener('click', deletBook);
+
+        function deletBook(e)
+        {
+        // Pass to UI 
+        const ui = new UI();
+        if(e.target.className === 'delete')
+        {
+            ui.deletSingleBook(e.target);
+            ui.showMessage('Book Removed!!','success');
+            e.preventDefault();
+        }
+
+        }
