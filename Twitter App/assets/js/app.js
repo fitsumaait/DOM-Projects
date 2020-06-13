cdaaa878a115e394063020189746b36edf84f962
Fitsum A <fitsum@gmail.com>
2020-06-13 22:37:32 +0300

    Adding the functionality for Loading and Adding to Local Storage

--- Twitter App/assets/js/app.js
+++ Twitter App/assets/js/app.js
@@ -7,7 +7,15 @@ const twwitList = document.querySelector('#tweet-list');
 loadEvents();
 function loadEvents()
 {
+    // add
     document.querySelector('#form').addEventListener('submit',addTwitt);
+    
+    // remove 
+    twwitList.addEventListener('click',removeTwitt);
+
+    // Dom load 
+    document.addEventListener('DOMContentLoaded',loadTwitts);
+
 }
 
 
@@ -20,11 +28,79 @@ function addTwitt(e)
     // Get the value 
     const twwit = document.querySelector('#tweet').value;
     
+    // Remove button 
+    const rmBtn= document.createElement('a');
+    rmBtn.classList = 'remove-tweet';
+    rmBtn.textContent = 'X';
     // create li
     const li = document.createElement('li');
-    li.textContent = twwit; 
+    li.textContent = twwit;
+
+    li.appendChild(rmBtn);
+
     twwitList.appendChild(li);
     
-    
+    // store 
+    addToStorage(twwit);
     e.preventDefault();
+}
+
+// remove twiits
+function removeTwitt(e)
+{
+     if(e.target.classList.contains('remove-tweet'))
+     {
+       e.target.parentElement.remove();
+     }
+
+
+}
+
+// Add to storage 
+function addToStorage(tw)
+{
+   let twitts;
+   if(localStorage.getItem('tweets')===null)
+   {
+       twitts = [];
+   }
+   else
+   {
+      twitts = JSON.parse(localStorage.getItem('tweets'));
+   
+    }
+    twitts.push(tw);
+
+    localStorage.setItem('tweets',JSON.stringify(twitts));
+}
+
+function loadTwitts()
+{
+    let twitts;
+    if(localStorage.getItem('tweets')===null)
+    {
+        twitts = [];
+    }
+    else
+    {
+       twitts = JSON.parse(localStorage.getItem('tweets'));
+    
+     }
+
+     twitts.forEach(element => 
+        {
+    
+    // Remove button 
+    const rmBtn= document.createElement('a');
+    rmBtn.classList = 'remove-tweet';
+    rmBtn.textContent = 'X';
+    // create li
+    const li = document.createElement('li');
+    li.textContent = element;
+
+    li.appendChild(rmBtn);
+
+    twwitList.appendChild(li);
+        }
+     );
 }
