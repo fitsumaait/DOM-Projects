73573b1af6f702b911b77f4b018c9743aefd93f9
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-13 22:45:45 +0530

    Few changes on the UI and Adding requred variables on the script.js

---
+++ Number Guess/assets/js/script.js
@@ -0,0 +1,10 @@
+// Game varibles 
+let min = 1 , max =10 , winNumber = 2 , guessLeft = 3;
+
+// UI variables 
+const game = document.querySelector('#game');
+const minNumber = document.querySelector('.min');
+const maxNumber = document.querySelector('.max');
+const guessInput = document.querySelector('#gInput');
+const guessBtn = document.querySelector('#gvalue');
+const msg = document.querySelector('.msg');
