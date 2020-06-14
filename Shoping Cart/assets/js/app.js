c42cb2929e3c774c91a5689c7ccde7b51ba34f6f
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-14 23:07:55 +0530

    Finsh the Add to cart functionality

--- Shoping Cart/assets/js/app.js
+++ Shoping Cart/assets/js/app.js
@@ -1,6 +1,6 @@
 // Variables 
 const courses = document.querySelector('#courses-list');
-
+const shopingcart = document.querySelector('#cart-content tbody');
 
 // Event Listeners 
 loadEvenets();
@@ -8,6 +8,7 @@ loadEvenets();
 function loadEvenets()
 {
      courses.addEventListener('click', addCourse);
+     shopingcart.addEventListener('click',removeCourse);
 
 
 
@@ -35,5 +36,49 @@ function addCourse(e)
 // Retrive course info 
 function getCourseInfo(course)
 {
+  const courseInfo= {
+      cImage: course.querySelector('img').src,
+      cTitle: course.querySelector('h4').textContent,
+      cPrice: course.querySelector('.price span').textContent,
+      cId: course.querySelector('a').getAttribute('data-id'),
+
+
+  }
+   addToCart(courseInfo);
+
+}
+// Adding to cart for display 
+function addToCart(courseInfo)
+{
+    // create tr 
+    const row = document.createElement('tr');
+
+    // Insert values 
+    row.innerHTML = `
+    <tr>   
+    <td>
+        <img src="${courseInfo.cImage}" width=100 />
+       </td>
+       <td>
+        ${courseInfo.cPrice}
+       </td>
+       <td>
+       <a href="#" class="remove" data-id="${courseInfo.cId}">X</a>
+      </td>
+      <tr>
+    `;
+    shopingcart.appendChild(row);
+
+}
+
+// remove course 
+function removeCourse(e)
+{
+    if(e.target.classList.contains('add-to-cart'))
+    {
+                e.target.parentElement.parentElement.remove();
+    }
+
+
 
 }
