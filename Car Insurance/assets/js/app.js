da14d368ab88f7f69f73c95e4c14a3ef4f80d576
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-15 01:15:46 +0530

    Adding the Year option to diplay on load

---
+++ Car Insurance/assets/js/app.js
@@ -0,0 +1,47 @@
+// vars 
+
+
+
+// Event
+
+document.addEventListener('DOMContentLoaded', function(){
+    // Creating options of year 
+    const html = new InsuranceUI()
+    html.displayYears();
+});
+
+
+
+
+
+
+
+
+
+// Objects and functions 
+function InsuranceUI()
+{
+
+
+}
+
+
+// proto
+InsuranceUI.prototype.displayYears = function()
+{
+    // Last 20 years 
+    const max = new Date().getFullYear(); 
+    const min = max - 20; 
+
+    // Generate the year 
+    const sYears = document.querySelector('#year');
+
+    for (let i =min; i<=max; i++)
+    {
+        const opt = document.createElement('option');
+        opt.value = i; 
+        opt.text = i;
+        sYears.appendChild(opt);
+    }
+}
+
